c8675a0385cc47df0c8721a4347cdf15dbad9b4c
Golerton <145386497+Goleron@users.noreply.github.com>
2024-03-30 03:02:45 +0300

    Add files via upload

---
+++ stranicaFE/stranicaFE/Artifact.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace stranicaFE
+{
+    /// <summary>
+    /// Логика взаимодействия для Artifact.xaml
+    /// </summary>
+    public partial class ArtifactPage : Page
+    {
+        private uchebEntities context = new uchebEntities();
+        public ArtifactPage()
+        {
+            InitializeComponent();
+            ArtifactDgr.ItemsSource = context.Artifacts.ToList();
+
+        }
+    }
+}
